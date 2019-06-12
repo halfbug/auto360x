9ad52daa61539756aceb4f69de48dab2812f83d8
Sadaf <sadaf_cu@hotmail.com>
2019-06-13 04:36:23 +0500

    fixed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/vehicles', vehicles_routes);
 // serve static assetps if in production
 if(process.env.NODE_ENV == 'production') {
     // set static folder
-    app.use(express.static('forntend/build'));
+    app.use(express.static('frontend/build'));
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
     } );
