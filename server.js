d93bf0e49bdbfd0e94fda2018d7b542200083291
Sadaf <sadaf_cu@hotmail.com>
2019-06-13 10:06:16 +0500

    build client

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV == 'production') {
     // set static folder
     app.use(express.static('frontend/build'));
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
     } );
 }
 
