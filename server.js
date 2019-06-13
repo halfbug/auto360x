e5a2b5864734a16accbd48385d47ef1988ba5679
Sadaf <sadaf_cu@hotmail.com>
2019-06-13 10:29:00 +0500

    serverjs fixed

--- server.js
+++ server.js
@@ -29,12 +29,12 @@ app.use('/api/vehicles', vehicles_routes);
 if(process.env.NODE_ENV == 'production') {
     // set static folder
     app.use(express.static('frontend/build'));
-    app.get('/*',(req,res) => {
-        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
     } );
 }
 
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>console.log(`Server started on port ${port}`))
